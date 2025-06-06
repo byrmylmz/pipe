026a9e06c865ef6eb15d66f6d1ce22e8f410eee8
byrmylmz <bayramyilmaz061@gmail.com>
2025-06-06 16:44:58 +0200

    update

--- pipe.c
+++ pipe.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdio.h>       // printf, perror
+#include <unistd.h>      // pipe, fork, read, write, close
+#include <string.h>      // strlen
+#include <sys/types.h>   // pid_t tipi
 
 int main() {
-    int pipefd[2]; // pipefd[0] = read, pipefd[1] = write
-    pid_t pid;
-    char buffer[100];
+    int pipefd[2];        // pipefd[0] = okuma ucu, pipefd[1] = yazma ucu
+    pid_t pid;            // process ID
+    char buffer[100];     // çocuk sürecin okuyacağı buffer
 
+    // Pipe oluştur
     if (pipe(pipefd) == -1) {
         perror("pipe");
         return 1;
     }
 
+    // fork ile çocuk süreç oluştur
     pid = fork();
 
     if (pid < 0) {
@@ -21,15 +23,17 @@ int main() {
     }
 
     if (pid == 0) {
-        // Çocuk süreç: okumayla ilgilenir
-        close(pipefd[1]);
-        read(pipefd[0], buffer, sizeof(buffer));
+        // --- Çocuk süreç: sadece okur ---
+        close(pipefd[1]); // yazma ucunu kapat
+        read(pipefd[0], buffer, sizeof(buffer)); // pipe'dan oku
         printf("Çocuk süreç okudu: %s\n", buffer);
+        close(pipefd[0]); // okuma ucunu kapat
     } else {
-        // Ebeveyn süreç: yazma işlemi yapar
-        close(pipefd[0]);
-        char *msg = "Merhaba çocuk!";
-        write(pipefd[1], msg, strlen(msg) + 1);
+        // --- Ana süreç: sadece yazar ---
+        close(pipefd[0]); // okuma ucunu kapat
+        const char *msg = "Merhaba çocuk!";
+        write(pipefd[1], msg, strlen(msg) + 1); // null byte dahil
+        close(pipefd[1]); // yazma ucunu kapat
     }
 
     return 0;
